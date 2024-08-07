c95dd0271ec64b259bf1ff62ba5cddf479cf16da
LDunke <159555008+LDunke@users.noreply.github.com>
2024-08-07 14:03:24 +0000

    [feat] Expo-font with Context

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,6 +1,11 @@
 import { Stack } from "expo-router";
+import { AppProvider } from "../hooks";
 
 
-export default function Layout () {
-    return <Stack />; 
+export default function Layout() {
+    return (
+    <AppProvider>
+        <Stack />
+    </AppProvider>
+    );
 }
