f82f266b1421cf1180e1ed9e4f7b462ed4617d3b
LDunke <159555008+LDunke@users.noreply.github.com>
2024-08-28 15:45:38 +0000

    [feat] Fim da Aula 7

--- src/hooks/Auth/index.js
+++ src/hooks/Auth/index.js
@@ -27,7 +27,10 @@ export function AuthProvider({ children }) {
                 user: null,
                 role: null,
             });
+            throw new Error("Usuário ou senha inválidos");
         }
+
+        console.log(response);
 
         setUser({
             autenticated: true,
