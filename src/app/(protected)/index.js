0ec035916c10361e16bfcfcbdd8dd8ac2116e51f
LDunke <159555008+LDunke@users.noreply.github.com>
2024-09-04 13:24:25 +0000

    [fix] Drawer Menu

--- src/app/(protected)/index.js
+++ src/app/(protected)/index.js
@@ -2,12 +2,11 @@ import { Button, Text, View } from "react-native";
 import { useAuth } from "../../hooks/Auth";
 
 export default function Home() {
-    const {signOut} = useAuth();
-    
+    const { signOut } = useAuth();
+
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home</Text>
-        <Button title="Sair" onPress={()=> signOut()} />
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home</Text>
         </View>
     );
 }
