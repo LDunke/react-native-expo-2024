9d72334fb311db53cbed230b024bca55abdb93b2
LDunke <159555008+LDunke@users.noreply.github.com>
2024-10-23 15:47:00 +0000

    começo aula 16

--- src/app/(protected)/list.js
+++ src/app/(protected)/list.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
 export default function List() {
+    const [payments, setPayments] = useState([
+        {id: 1, user_id: 1},
+        {id: 2, user_id: 2},
+        {id: 3, user_id: 3},
+        {id: 4, user_id: 4},
+    ]);
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#e0f2f1' }}>
-            <Text style={{ fontSize: 24, fontWeight: '600', color: 'green' }}>Listagem</Text>
-            <Text style={{ fontSize: 24, fontWeight: '600', color: 'green' }}></Text>
+            <View style={{flex: 1}}>
+                <FlatList 
+                data={payments}
+                keyExtractor={item => item.id.toString()}
+                renderItem={({item}) => <Text>{item.user_id}</Text>}
+                />
+            </View>
         </View>
     );
 }
--- src/database/useUsersDatabase.js
+++ src/database/useUsersDatabase.js
@@ -31,5 +31,4 @@ export function useUsersDatabase() {
     return {
         authUser,
         getAllUsers,
-    };
-}
+    }; }
